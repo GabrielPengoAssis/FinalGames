8bfdece3ff70caf6eff76056413a52bf4abe0761
jppaschoalini <joao.paschoalini@sou.unaerp.edu.br>
2025-11-18 21:15:07 -0300

    script gasolina

--- My project/Assets/Scripts/ScoreManager.cs
+++ My project/Assets/Scripts/ScoreManager.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+    public GameObject player;
     public Text uiScore; // Referência ao componente UI Text
     public Text uiFinalScore; // Referência ao componente UI Text
     public Text uiRestarting; // Referência ao componente UI Text
+    public Text uiCombustivel;
+    public Image Combustivel_imagem;
+    public RectTransform barraCombustivel; // arraste a imagem aqui no Inspector
     public float Points = 0;
     public float Multiplier = 1;
     public float FinalScore = 0;
+    public float Combustivel = 100;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -21,12 +26,14 @@ public class ScoreManager : MonoBehaviour
             Destroy(other.gameObject);
             Points += 100;       // Adiciona 100 pontos            
             Multiplier += 0.1f;   // Aumenta o multiplicador
+            Combustivel += 10;
         }
 
         if (other.gameObject.CompareTag("Red"))
         {
             Destroy(other.gameObject);
             Multiplier = 1;   // reseta o multiplicador
+            Combustivel -= 3;
         }
 
         if (other.gameObject.CompareTag("Blue"))
@@ -42,6 +49,10 @@ public class ScoreManager : MonoBehaviour
 
     private IEnumerator ReiniciarCenaComDelay(float delay)
     {
+        player.GetComponent<CarController>().enabled = false;
+        uiScore.enabled = false;
+        uiCombustivel.enabled = false;
+        Combustivel_imagem.enabled = false;
         Debug.Log("Reiniciando em " + delay + " segundos...");
         yield return new WaitForSeconds(delay);
         SceneManager.LoadScene("Titulo");
@@ -49,8 +60,22 @@ public class ScoreManager : MonoBehaviour
 
     private void Update()
     {
+        Combustivel = Combustivel - 0.1f;
+
+        Vector2 tamanho = barraCombustivel.sizeDelta;
+        tamanho.y = Combustivel;
+        barraCombustivel.sizeDelta = tamanho;
+
         // Atualiza o texto da UI a cada frame
         uiScore.text = $"{Points}pts {Multiplier}x";
+        uiCombustivel.text = $"Combustivel:{Combustivel}";
+        if(Combustivel <= 0)
+        {
+            uiFinalScore.text = $"{FinalScore}pts"; // mostra o score final
+            uiRestarting.text = $"reiniciando...";
+            StartCoroutine(ReiniciarCenaComDelay(5f));
+
+        }
 
 
     }
